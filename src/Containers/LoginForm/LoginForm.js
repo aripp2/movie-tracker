3edcdf6d5929a1a0a187891eacc8c7cb744f3af1
Eduardo <jeduardorjx@gmail.com>
2019-10-23 15:24:18 -0600

    Create out basic form

---
+++ src/Containers/LoginForm/LoginForm.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import './LoginForm.scss'
+
+class LoginForm extends Component {
+  constructor() {
+    super();
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <section>
+        <form>
+          form
+        </form>
+      </section>
+    )
+  } 
+}
+
+export default LoginForm;

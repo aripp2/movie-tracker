6950d840fdefa59bf1aaf8c24a989c6d0a9fe045
Eduardo <jeduardorjx@gmail.com>
2019-10-23 16:50:39 -0600

    Trying to get our Alan user

--- src/Containers/LoginForm/LoginForm.js
+++ src/Containers/LoginForm/LoginForm.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react'
 import './LoginForm.scss'
 import { Link } from 'react-router-dom'
+import { postUser } from '../../utils/apiCalls'
 
 
 class LoginForm extends Component {
   constructor() {
     super();
     this.state = {
-
     }
   }
 
   render() {
+
+    postUser('http://localhost:3001/api/v1/login')
     return (
       <section className='form-section'>
         <form>

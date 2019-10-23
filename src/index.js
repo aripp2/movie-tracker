038b5388289e51633916ba93377db13cf85118d5
Jon O'Drobinak <jodrobinak@gmail.com>
2019-10-23 09:59:06 -0600

    Update main index file with redux store info

--- src/index.js
+++ src/index.js
@@ -4,11 +4,17 @@ import './index.scss';
 import App from './App/App';
 import * as serviceWorker from './serviceWorker';
 
-import { Provider } from 'react-redux'
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import rootReducer from './reducers';
+
+const store = createStore(rootReducer, composeWithDevTools());
 
 ReactDOM.render(
-<Provider>
-  <App /> document.getElementById('root'));
+<Provider store={store}>
+  <App /> 
 </Provider>,
+document.getElementById('root'));
 
 serviceWorker.unregister();

7e936a881703e647076efd4f9f9878c2889f0397
Eduardo <jeduardorjx@gmail.com>
2019-10-23 15:23:58 -0600

    Import login fom

--- src/Containers/App/App.js
+++ src/Containers/App/App.js
@@ -5,6 +5,7 @@ import PropTypes from 'prop-types';
 import { fetchMovies } from '../../utils/apiCalls';
 import MoviesContainer from '../MoviesContainer/MoviesContainer';
 import './App.scss';
+import LoginForm from '../LoginForm/LoginForm'
 
 class App extends Component {
   constructor() {
@@ -26,10 +27,10 @@ class App extends Component {
 
   render() {
     const { movies } = this.state
-    console.log(movies)
     return (
       <div className="App">
         <header className="App-header">Movie Tracker</header>
+        <LoginForm />
         <MoviesContainer movies={movies}/>
       </div>
     );

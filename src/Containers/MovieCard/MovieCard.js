7e58980ad67b13bc4375d2d6d3b8c0b73c0ccd52
Eduardo <jeduardorjx@gmail.com>
2019-10-26 10:09:54 -0600

    Changed our html tags so it's semantic, removed the overview and vote info, placed image tag first

--- src/Containers/MovieCard/MovieCard.js
+++ src/Containers/MovieCard/MovieCard.js
@@ -3,18 +3,17 @@ import  './MovieCard.scss';
 import { toggleFavorite } from '../../actions'
 import { connect } from 'react-redux'
 
-const MovieCard = ({ id, title, date, poster, vote, overview, isFavorite, toggleFav }) => {
+const MovieCard = ({ id, title, date, poster, isFavorite, toggleFav }) => {
   const fixedDate = new Date(date + 'T00:00').toString().split(' ').slice(1, 4).join(' ');
+
   return (
-    <div className='movie-card' id={id}>
-      <h3>{title}</h3>
-      <h3>{fixedDate}</h3>
+    <article className='movie-card' id={id}>
       <img className='poster' src={`http://image.tmdb.org/t/p/w1280${poster}`} alt='movie poster'/>
-      <h3>{vote}</h3>
-      <p>{overview}</p>
+      <h2>{title}</h2>
+      <h3>{fixedDate}</h3>
       <button type="button"
       onClick={() => toggleFav(id) }>Add Favorite</button>
-    </div>
+    </article>
   )
 }
 

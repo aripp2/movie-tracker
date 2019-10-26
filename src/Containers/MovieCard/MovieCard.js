6e0bafc74865069fdd4010963ca364be9eaa94d4
Eduardo <jeduardorjx@gmail.com>
2019-10-26 10:03:44 -0600

    Import our toggleFavorite action, set our mapDispatchToProps, and set a onClick on our button using toggleFav

--- src/Containers/MovieCard/MovieCard.js
+++ src/Containers/MovieCard/MovieCard.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import  './MovieCard.scss';
+import { toggleFavorite } from '../../actions'
+import { connect } from 'react-redux'
 
-const MovieCard = ({ id, title, date, poster, vote, overview }) => {
+const MovieCard = ({ id, title, date, poster, vote, overview, isFavorite, toggleFav }) => {
   const fixedDate = new Date(date + 'T00:00').toString().split(' ').slice(1, 4).join(' ');
   return (
     <div className='movie-card' id={id}>
@@ -10,9 +12,14 @@ const MovieCard = ({ id, title, date, poster, vote, overview }) => {
       <img className='poster' src={`http://image.tmdb.org/t/p/w1280${poster}`} alt='movie poster'/>
       <h3>{vote}</h3>
       <p>{overview}</p>
-      <button type="button">Add Favorite</button>
+      <button type="button"
+      onClick={() => toggleFav(id) }>Add Favorite</button>
     </div>
   )
 }
 
-export default MovieCard;
+const mapDispatchToProps = dispatch => ({
+  toggleFav: (id) => dispatch( toggleFavorite(id))
+})
+
+export default connect(null, mapDispatchToProps)(MovieCard);

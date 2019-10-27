e8db395a0e70ca0f0eca5caa1dae4bf7736d273b
Amy Rippeto <amyrippeto@gmail.com>
2019-10-26 19:24:39 -0600

    Work on sign out logic

--- src/Containers/Header/Header.js
+++ src/Containers/Header/Header.js
@@ -1,18 +1,42 @@
 import React from 'react';
-import './Header';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { setUser } from '../../actions';
+import './Header.scss';
 
-const Header = () => {
+const Header = ({ user }) => {
+    console.log('in header', user)
+    console.log('in header props', setUser)
+
   return (
     <header>
+      <button>View Favorites</button>
       <h1>Movie Tracker</h1>
-      <Link to='/login'><button className='login'>Login</button></Link>
-      <Link to='/createaccount'><button className='create'>Create Account</button></Link>
-      <Link to='/'><button className='signout'>Sign Out</button></Link>
-
-
+      {user !== null && <h2>Welcome {user.name}</h2>}
+      <Link to='/login'>
+        <button 
+          className='login'
+          >Login</button>
+        </Link>
+      <Link to='/createaccount'>
+        <button 
+          className='create'
+          >Create Account</button>
+      </Link>
+      <button 
+        className='signout'
+        onClick={() => setUser()}
+      >Sign Out</button>
     </header>
   )
 }
 
-export default Header;
+const mapStateToProps = ({ user }) => ({
+  user
+})
+
+const mapDispatchToProps = dispatch => ({
+  setUser: user => dispatch(setUser(user))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
--- src/reducers/user.js
+++ src/reducers/user.js
@@ -6,3 +6,4 @@ export const user = (state = null, action) => {
       return state
   }
 }
+

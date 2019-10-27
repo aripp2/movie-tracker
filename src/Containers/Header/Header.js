42439f13dad60759622700b7804be0125cacc585
Eduardo <jeduardorjx@gmail.com>
2019-10-26 19:45:58 -0600

    Import the header scss file

--- src/Containers/Header/Header.js
+++ src/Containers/Header/Header.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import './Header';
+import './Header.scss';
 import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
     <header>
-      <h1>Movie Tracker</h1>
+      <h1 className='header-title'>Movie Tracker</h1>
       <Link to='/login'><button className='login'>Login</button></Link>
       <Link to='/createaccount'><button className='create'>Create Account</button></Link>
       <Link to='/'><button className='signout'>Sign Out</button></Link>

6a0fb94bcf6762b24d1f33a2e168a20d3c3409e7
Eduardo <jeduardorjx@gmail.com>
2019-10-26 19:43:32 -0600

    wrap the movie detials in a div

--- src/Containers/MovieCard/MovieCard.js
+++ src/Containers/MovieCard/MovieCard.js
@@ -8,11 +8,13 @@ const MovieCard = ({ id, title, date, poster, isFavorite, toggleFav }) => {
 
   return (
     <article className='movie-card' id={id}>
-      <img className='poster' src={`http://image.tmdb.org/t/p/w1280${poster}`} alt='movie poster'/>
-      <h2>{title}</h2>
-      <h3>{fixedDate}</h3>
-      <button type="button"
-      onClick={() => toggleFav(id) }>Add Favorite</button>
+      <div className='movie-details'>
+        <h2>{title}</h2>
+        <h3>{fixedDate}</h3>
+        <button className='fav-btn' type="button"
+        onClick={() => toggleFav(id) }>Add Favorite</button>
+      </div>
+        <img className='poster' src={`http://image.tmdb.org/t/p/w1280${poster}`} alt='movie poster'/>
     </article>
   )
 }

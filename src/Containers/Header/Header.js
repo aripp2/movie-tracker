844c0ddb448e24a6f661205ebf5faf39c7d2b416
Eduardo <jeduardorjx@gmail.com>
2019-10-28 15:27:23 -0600

    Added a wrapper to the username and buttons

--- src/Containers/Header/Header.js
+++ src/Containers/Header/Header.js
@@ -9,31 +9,33 @@ const Header = ({ user, setUser, setFavs }) => {
   return (
     <header>
       <h1 className='header-title'>Movie Tracker</h1>
-      {user !== null && <h2>Welcome {user.name}</h2>}
-      <Link to='/login'>
-        <button 
-          className='login nav-btn'
-          disabled={user}
-          >Login</button>
+      <div className='header-btn-wrapper'>
+        {user !== null && <h2>Welcome {user.name}</h2>}
+        <Link to='/login'>
+          <button 
+            className='login nav-btn'
+            disabled={user}
+            >Login</button>
+          </Link>
+        <Link to='/createaccount'>
+          <button 
+            className='create nav-btn'
+            disabled={user}
+            >Create Account</button>
         </Link>
-      <Link to='/createaccount'>
         <button 
-          className='create nav-btn'
-          disabled={user}
-          >Create Account</button>
-      </Link>
-      <button 
-        className='signout nav-btn'
-        disabled={!user}
-        onClick={() => {
-          setUser(null);
-          setFavs([]);
-        }}
-      >Sign Out</button>
-      <button 
-        className='view-favs nav-btn'
-        disabled={!user}
-      >View Favorites</button>
+          className='signout nav-btn'
+          disabled={!user}
+          onClick={() => {
+            setUser(null);
+            setFavs([]);
+          }}
+        >Sign Out</button>
+        <button 
+          className='view-favs nav-btn'
+          disabled={!user}
+        >View Favorites</button>
+      </div>
     </header>
   )
 }

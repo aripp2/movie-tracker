ea0dd79d356193e8693bdc70438040a7184441db
Jon O'Drobinak <jodrobinak@gmail.com>
2019-10-29 17:04:21 -0600

    Add tests to LoginForm

--- src/Containers/LoginForm/LoginForm.test.js
+++ src/Containers/LoginForm/LoginForm.test.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import { LoginForm, mapStateToProps, mapDispatchToProps } from './LoginForm';
-import { submitUser } from '../../actions/index';
+import { setUser } from '../../actions/index';
 
 describe('LoginForm', () => {
-  let wrapper, mockUser, mockValid;
+  let wrapper, mockUser, mockValidUser;
 
   beforeEach(() => {
     mockUser = null;
@@ -16,4 +16,45 @@ describe('LoginForm', () => {
       />);
 
   });
+
+  it('should match teh snapshot with all the data passed in correctly', () => {
+
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should update state on a change in an input', () => {
+
+    const mockEvent = {target: {name: 'email', value: 'alan@turing.io'}}
+
+    wrapper.find('input').at(0).simulate('change', mockEvent);
+
+    expect(wrapper.state('email')).toEqual('alan@turing.io')
+  });
+
+  it('should update state on a change in an input', () => {
+
+    const mockEvent = { target: { name: 'password', value: 'password' } }
+   
+    wrapper.find('input').at(1).simulate('change', mockEvent);
+
+    expect(wrapper.state('password')).toEqual('password');
+  });
+
+  
+
+  it('should update the state when clearInputs is called', () => {
+
+    const expected = '';
+
+    wrapper.setState({
+      email: 'alan@turing.io',
+      password: 'password',
+    });
+
+    wrapper.instance().clearInputs();
+
+    expect(wrapper.state('email')).toEqual(expected);
+    expect(wrapper.state('password')).toEqual(expected);
+  });
+
 });

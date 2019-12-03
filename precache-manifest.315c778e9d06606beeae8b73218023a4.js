1bf2ec1648c2fda689fa30cad01a1f28a410b462
Amy Rippeto <amyrippeto@gmail.com>
2019-12-03 14:48:33 -0700

    Updates

---
+++ precache-manifest.315c778e9d06606beeae8b73218023a4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9542c4ab460dac56ff8f0e9d6af8993c",
+    "url": "/movie-tracker/index.html"
+  },
+  {
+    "revision": "9b2322423ea0973bd93e",
+    "url": "/movie-tracker/static/css/main.be113708.chunk.css"
+  },
+  {
+    "revision": "c955dca5961b4093a242",
+    "url": "/movie-tracker/static/js/2.901f840c.chunk.js"
+  },
+  {
+    "revision": "9b2322423ea0973bd93e",
+    "url": "/movie-tracker/static/js/main.405453da.chunk.js"
+  },
+  {
+    "revision": "9179fc866cc297eda30b",
+    "url": "/movie-tracker/static/js/runtime-main.33096d0c.js"
+  }
+]);
